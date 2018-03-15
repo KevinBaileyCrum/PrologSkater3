f11317ec827c45a3251b526c9035845e70575a1c
Kevin Bailey Crum <kevinbaileycrum@gmail.com>
2018-03-15 02:18:40 -0700

    graph path stops at first successful path

--- functions.pl
+++ functions.pl
@@ -1,5 +1,50 @@
 % $Id: functions.pl, Kevin Crum - CMPS 112 -- ASG4
 
+
+% $Id: graphpaths.pl,v 1.3 2011-05-19 19:53:59-07 - - $ */
+
+%
+% Prolog version of not.
+%
+
+not( X ) :- X, !, fail.
+not( _ ).
+
+%
+% Is there a path from one node to another?
+%
+
+
+%
+% Find a path from one node to another.
+%
+
+fly( Node, Node ) :-
+   write( Node ), write( ' is ' ), write( Node ), nl.
+fly( Node, Next ) :-
+   listpath( Node, Next, [Node], List ),
+   write( Node ), write( ' to ' ), write( Next ), write( ' is ' ),
+   writepath( List ),
+   fail.
+
+writepath( [] ) :-
+   nl.
+writepath( [Head|Tail] ) :-
+   write( ' ' ), write( Head ), writepath( Tail ).
+
+listpath( Node, End, Outlist ) :-
+   listpath( Node, End, [Node], Outlist ).
+
+listpath( Node, Node, _, [Node] ).
+listpath( Node, End, Tried, [Node|List] ) :-
+   flight( Node, Next, time( Hour, Min ) ),
+   not( member( Next, Tried )),
+   % cut at the end of vvv list path below stops at first success
+   listpath( Next, End, [Next|Tried], List ),!.
+
+
+/*
+
 mathfns( X, List ) :-
    S is sin( X ),
    C is cos( X ),
@@ -25,16 +70,15 @@ haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
    write( Distance ), 
    nl.
 
- %minToDeg( M ) :-
 
 % isAirport( X )
 % Takes in airport abreviation and finds corresponding
 % values of Latitude and Longitude.  It converts
 % said values minutes -> degrees -> radians.
-%   DegAndMin1 is first lat converted and summed
+%   min = 1deg/60     rad = deg*pi/180 
 isAirport( X ) :-
   airport( X, Name, degmin( Deg1, Min1), degmin( Deg2, Min2)), nl,
-  
+      
   % Get North Latitude of X
   write(Deg1), nl,
   write(Min1), nl,
@@ -43,15 +87,15 @@ isAirport( X ) :-
   DegAndMin1 is Deg1 + Frac1,
   write(DegAndMin1), nl,
   D1asPi is DegAndMin1 *(pi / 180),
-  write('North Lat in Radians '), write(D1asPi), nl,
+  write('North Lat in Radians for X '), write(D1asPi), nl,
 
   % Get West Longitutde of X
   Frac2 is Min2 / 60,
   DegAndMin2 is Deg2 + Frac2,
   D2asPi is DegAndMin2 *(pi / 180),
-  write('West Long in Radians '), write(D2asPi), nl.
-
-
+  write('West Long in Radians for X '), write(D2asPi), nl.
+  
+  % Get North Latitude of Y
   %airport( Y, Name, degmin( yDeg1, yMin1), degmin( yDeg2, yMin2)), nl,
   %write(yDeg1), nl,
   %write(yMin1),
@@ -60,13 +104,13 @@ isAirport( X ) :-
 
 
 % is sea -- group0.test asks fly(sea,lax)
-fly( X, Y ) :-
-   % write('this q'),
-   % write('y equal'),
-   write(Y), nl,
-   isAirport( X ).
- 
+%fly( X, Y ) :-
+  % write('this q'),
+  % write('y equal'),
+  %write(Y), nl,
+  %isAirport( X ).
+*/    
 
 
 
-% 60 min = 1 deg.  (deg= deg + min/60?) rad=pi/180 * deg
+
