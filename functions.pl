0465d125ca8321d3841c0476d28db433fdcf4508
Kevin Bailey Crum <kevinbaileycrum@gmail.com>
2018-03-14 19:42:25 -0700

    got lat and long of first X converted to radians

--- functions.pl
+++ functions.pl
@@ -34,12 +34,30 @@ haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
 %   DegAndMin1 is first lat converted and summed
 isAirport( X ) :-
   airport( X, Name, degmin( Deg1, Min1), degmin( Deg2, Min2)), nl,
+  
+  % Get North Latitude of X
   write(Deg1), nl,
   write(Min1), nl,
   Frac1 is Min1 / 60,
   write(Frac1), nl,
   DegAndMin1 is Deg1 + Frac1,
-  write(DegAndMin1).
+  write(DegAndMin1), nl,
+  D1asPi is DegAndMin1 *(pi / 180),
+  write('North Lat in Radians '), write(D1asPi), nl,
+
+  % Get West Longitutde of X
+  Frac2 is Min2 / 60,
+  DegAndMin2 is Deg2 + Frac2,
+  D2asPi is DegAndMin2 *(pi / 180),
+  write('West Long in Radians '), write(D2asPi), nl.
+
+
+  %airport( Y, Name, degmin( yDeg1, yMin1), degmin( yDeg2, yMin2)), nl,
+  %write(yDeg1), nl,
+  %write(yMin1),
+
+  
+
 
 % is sea -- group0.test asks fly(sea,lax)
 fly( X, Y ) :-
